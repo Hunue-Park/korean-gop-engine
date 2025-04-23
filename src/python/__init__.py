83307fbb9eca84863d333f9a224a0b5e9bd9a19c
hunoo-park <hw.park@teamsparta.co>
2025-04-23 14:46:54 +0900

    fix: 폴더 정리

--- src/python/__init__.py
+++
@@ -1,5 +0,0 @@
-# 주요 인터페이스 노출
-from .engine.korean_engine import KoreanSpeechEngine
-from .engine.engine_factory import get_engine_instance
-
-__all__ = ['KoreanSpeechEngine', 'get_engine_instance']
